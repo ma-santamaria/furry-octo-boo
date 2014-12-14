9dd6f03db6d492c65c4d233ff2d1220a20f0a957
Miguel Ángel Santamaría Rogado <ma.santamaria.rogado@gmail.com>
2014-12-14 01:52:23 +0100

    Commit después de conflicto

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         NSLog(@"Hola Caracola")
         NSLog(@"Añadido desde Xcode. Para prueba de conflicto")
+        NSLog(@"Añadido para segunda prueba")
     }
 
     override func didReceiveMemoryWarning() {
